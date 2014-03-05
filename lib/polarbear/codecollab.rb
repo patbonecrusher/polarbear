7dd7d37ec82f83aebeaf58be690619ff7b176f62
Bone Crusher <pat@covenofchaos.com>
2014-03-04 20:00:12 -0500

    cleaned up codecollaborator class

--- lib/polarbear/codecollab.rb
+++ lib/polarbear/codecollab.rb
@@ -29,11 +29,8 @@ module PolarBear
 
     # --------------------------------------------------------------------------
     # --------------------------------------------------------------------------
-    # @param [Object] options
-    def initialize(options = {})
-
-      @options = options
-      validate_and_set_options(options) unless options.empty?
+    # @return [Object]
+    def initialize
 
       @ccollab_execpath = find_ccollab_executable
       Utils::Executor.instance.set_codecollab_exec_path(@ccollab_execpath)
@@ -69,34 +66,6 @@ module PolarBear
       Review.new(review_hash[:reviews][:review][:general])
     end
 
-    # @param [Object] review_data
-    def create_review(review_data = {})
-      puts review_data
-
-      options = PolarBear::Command::GlobalOptions.new()
-      batch = PolarBear::Command::Batch.new(options)
-
-      batch.add_command(':admin_review_create', {':title' => "#{review_data[:title]}"}) if (review_data.has_key?(:title))
-      batch.add_command(':admin_review_create/', '') if (!review_data.has_key?(:title))
-
-      if review_data.has_key?(:reviewers) || review_data.has_key?(:observers)
-        add_participants_content = { }
-        add_participants_content.compare_by_identity
-        review_data[:reviewers].each {|reviewer| add_participants_content[':participant'] = "reviewer=#{reviewer}" }
-        review_data[:observers].each {|observer| add_participants_content[':participant'] = "observer=#{observer}" }
-        add_participants_content[':review'] = 'last'
-        batch.add_command(':admin_review_set-participants', add_participants_content)
-        batch.add_command(':admin_review-xml', {':review' => 'last'})
-      end
-
-      batch.add_command(':admin_review-xml', {':review' => 'last'})
-
-      review_xml = @admin.execute_batch_command(batch)
-      parser = Nori.new(:convert_tags_to => lambda { |tag| tag.snakecase.to_sym })
-      review_hash = parser.parse(review_xml)
-      Review.new(review_hash[:reviews][:review])
-    end
-
     def delete_reviews(reviews)
       options = PolarBear::Command::GlobalOptions.new()
       batch = PolarBear::Command::Batch.new(options)
@@ -106,42 +75,17 @@ module PolarBear
         batch.add_command('admin_review_cancel', {':review' => "#{review.id}"})
       end
 
-      @admin.execute_batch_command(batch)
+      batch.execute
     end
 
     def login(url, username, password)
       Utils::Executor.instance.execute_command("login #{url} #{username} #{password}")
     end
 
-    def logout(url, username, password)
-      @admin.execute_command("login #{url} #{username} #{password}")
+    def logout
+      Utils::Executor.instance.execute_command('logout')
     end
 
-    private
-
-    # This validates that the keys are valid. If they are, it sets the value
-    # of that key's corresponding method to the given value. If a key ends
-    # with a '?', it's validated as a File method.
-    #
-    def validate_and_set_options(options)
-      options.each do |key, value|
-        key = key.to_s.downcase
-
-        if key[-1].chr == '?'
-          sym = key.to_sym
-
-          unless CodeCollab.respond_to?(sym)
-            raise ArgumentError, "invalid option '#{key}'"
-          end
-        else
-          unless VALID_OPTIONS.include?(key)
-            raise ArgumentError, "invalid option '#{key}'"
-          end
-
-          send("#{key}=", value)
-        end
-      end
-    end
 
   end
 
